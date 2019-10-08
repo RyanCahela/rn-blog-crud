bdf5fc56f69f4b4dbb308453cf63f2886daf0091
ryancahela <ryancahela@gmail.com>
2019-10-08 09:17:47 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
+
+
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+  Show: ShowScreen,
+  Create: CreateScreen,
+  Edit: EditScreen,
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blog-Crud',
+  }
+});
+
+export default createAppContainer(navigator);
